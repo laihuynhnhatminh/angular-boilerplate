ee7a9b471f51e34013aa3ce3370fb0c650d6118b
Minh Lai Huynh Nhat <mlaihuynhnhat@openwt.com>
2023-10-20 17:05:25 +0700

    Config eslint-prettier to the boiletplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-boilerplate';
+
+  text_fake = 'angular-boilerplate';
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:3000/api/v1',
+  production: false,
+  apiUrl: 'http://localhost:3000/api/v1',
 };
